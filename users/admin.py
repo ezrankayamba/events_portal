85beb9c0d045b8b9601b10af9e85e97edfd1ab09
ezrankayamba <ezrankayamba@gmail.com>
2019-09-04 17:52:59 +0300

    Added user models

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Permission, Role
 
 admin.site.register(Profile)
+admin.site.register(Permission)
+admin.site.register(Role)
--- users/models.py
+++ users/models.py
@@ -3,12 +3,32 @@ from django.contrib.auth.models import User
 from PIL import Image
 
 
+class Permission(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    is_internal = models.BooleanField(default=False)
+    permissions = models.ManyToManyField(Permission)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    role = models.ForeignKey(Role, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username}'
 
     def save(self, *args, **kwargs):
         super(Profile, self).save(*args, **kwargs)
