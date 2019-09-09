324c0372086b1c16a8d70e4c04dab50ab2c69d14
ezrankayamba <ezrankayamba@gmail.com>
2019-09-09 23:00:57 +0300

    Added readmails script

--- base/apps.py
+++ base/apps.py
@@ -1,5 +1,12 @@
 from django.apps import AppConfig
+from nots.views import main as readmail
+import time
 
 
 class BaseConfig(AppConfig):
     name = 'base'
+    verbose_name = "Base Application"
+
+    def ready(self):
+        print(f'The base application is now running')
+        readmail()
--- nots/views.py
+++ nots/views.py
@@ -4,12 +4,14 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+import time
 
-# If modifying these scopes, delete the file token.pickle.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+LABEL_SUCCESS = 'ReadMails/IOP1002/Success'
+LABEL_FAIL = 'ReadMails/IOP1002/Fail'
 
 
 def main():
+    SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
@@ -18,20 +20,36 @@ def main():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
+    service = build('gmail', 'v1', credentials=creds)
+    labels = my_labels(service)
+    while True:
+        print('Reading mail...')
+        # results = service.users().messages().list(userId='me', labelIds=['INBOX'], q='is:unread').execute()
+        results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+        messages = results.get('messages', [])
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            print(msg['snippet'])
+            msg_labels = {'removeLabelIds': ['INBOX'], 'addLabelIds': [labels['success']]}
+            service.users().messages().modify(userId='me', id=message['id'], body=msg_labels).execute()
+            # break
+        time.sleep(15)
 
-    gmail = build('gmail', 'v1', credentials=creds)
 
-    results = gmail.users().labels().list(userId='me').execute()
+def my_labels(service):
+    results = service.users().labels().list(userId='me').execute()
     labels = results.get('labels', [])
-
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+    labels = list(filter(lambda x: x['name'] == LABEL_SUCCESS or x['name'] == LABEL_FAIL, labels))
+    if len(labels) >= 2:
+        res = {}
+        for x in labels:
+            if x['name'] == LABEL_SUCCESS:
+                res['success'] = x['id']
+            elif x['name'] == LABEL_FAIL:
+                res['fail'] = x['id']
+        return res
+    return None
