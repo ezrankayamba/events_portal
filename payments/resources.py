660461929a2ab9b52bf17edd0c946c7fcf161581
ezrankayamba <ezrankayamba@gmail.com>
2019-09-13 18:21:35 +0300

    Fixe

--- payments/resources.py
+++ payments/resources.py
@@ -1,6 +1,9 @@
 from import_export import resources
 from .models import Payment, Ticket
+from companies.models import CompanyUser, Region
 from django.db.models import F
+from import_export import fields, resources
+from import_export.widgets import ForeignKeyWidget
 
 
 class PaymentResource(resources.ModelResource):
@@ -9,6 +12,21 @@ class PaymentResource(resources.ModelResource):
 
 
 class TicketResource(resources.ModelResource):
+    trans_id = fields.Field(column_name='Trans ID', attribute='payment', widget=ForeignKeyWidget(Payment, 'trans_id'))
+    issuer = fields.Field(column_name='Issuer', attribute='issuer', widget=ForeignKeyWidget(CompanyUser, 'user__username'))
+    region = fields.Field(column_name='Region', attribute='region', widget=ForeignKeyWidget(Region, 'name'))
+    issue_date = fields.Field(column_name='Issue Date', attribute='issue_date')
+    unit_price = fields.Field(column_name='Unit Price', attribute='unit_price')
+    ticket_value = fields.Field(column_name='Amount', attribute='ticket_value')
+    ticket_count = fields.Field(column_name='Ticket Count', attribute='ticket_count')
+    balance = fields.Field(column_name='Balance', attribute='balance')
+
     class Meta:
         model = Ticket
-        fields = ['payment__trans_id', 'region__name', 'issue_date', 'ticket_value', 'unit_price', 'ticket_count', 'balance', 'issuer__user__username']
+        fields = ['trans_id', 'region', 'issue_date', 'ticket_value', 'unit_price', 'ticket_count', 'balance', 'issuer']
+        widgets = {
+            'Issue Date': {'format': '%d.%m.%Y'},
+        }
+
+    def get_queryset(self):
+        return Ticket.objects.all()
