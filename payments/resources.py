7299628e9a698d646a717500c6d137146225b664
ezrankayamba <ezrankayamba@gmail.com>
2019-09-14 18:05:39 +0300

    Fixed export issues related to formating and columns

--- payments/resources.py
+++ payments/resources.py
@@ -3,7 +3,8 @@ from .models import Payment, Ticket
 from companies.models import CompanyUser, Region
 from django.db.models import F
 from import_export import fields, resources
-from import_export.widgets import ForeignKeyWidget
+from import_export import widgets
+# from import_export.widgets import ForeignKeyWidget
 
 
 class PaymentResource(resources.ModelResource):
@@ -13,38 +14,67 @@ class PaymentResource(resources.ModelResource):
     payer_account = fields.Field(
         column_name='Payer MSISDN', attribute='payer_account')
     payer_name = fields.Field(column_name='Payer Name', attribute='payer_name')
-    trans_date = fields.Field(column_name='Trans Date', attribute='trans_date')
+    trans_date = fields.Field(column_name='Trans Date', attribute='trans_date',
+                              widget=widgets.DateTimeWidget(format='%d/%m/%Y %H:%M'))
     amount = fields.Field(column_name='Amount', attribute='amount')
     ticket_issued = fields.Field(
         column_name='Ticket Issued', attribute='ticket_issued')
+
+    def __init__(self, user):
+        self.user = user
 
     class Meta:
         model = Payment
         fields = ['trans_id', 'channel', 'receipt_no', 'payer_account', 'payer_name', 'trans_date',
                   'amount', 'ticket_issued']
 
+    def get_queryset(self):
+        if self.user:
+            role = self.user.profile.role
+            if role:
+                if role.is_internal:
+                    return Payment.objects.all()
+                else:
+                    company = self.user.companyuser.company
+                    print('Company: ', company)
+                    return Payment.objects.filter(company=company)
+        print('Not allowed to export!')
+        return []
+
 
 class TicketResource(resources.ModelResource):
     trans_id = fields.Field(column_name='Trans ID', attribute='payment',
-                            widget=ForeignKeyWidget(Payment, 'trans_id'))
+                            widget=widgets.ForeignKeyWidget(Payment, 'trans_id'))
     issuer = fields.Field(column_name='Issuer', attribute='issuer',
-                          widget=ForeignKeyWidget(CompanyUser, 'user__username'))
+                          widget=widgets.ForeignKeyWidget(CompanyUser, 'user__username'))
     region = fields.Field(column_name='Region', attribute='region',
-                          widget=ForeignKeyWidget(Region, 'name'))
-    issue_date = fields.Field(column_name='Issue Date', attribute='issue_date')
+                          widget=widgets.ForeignKeyWidget(Region, 'name'))
+    issue_date = fields.Field(column_name='Issue Date', attribute='issue_date',
+                              widget=widgets.DateTimeWidget(format='%d/%m/%Y %H:%M'))
     unit_price = fields.Field(column_name='Unit Price', attribute='unit_price')
     ticket_value = fields.Field(column_name='Amount', attribute='ticket_value')
     ticket_count = fields.Field(
         column_name='Ticket Count', attribute='ticket_count')
     balance = fields.Field(column_name='Balance', attribute='balance')
 
+    def __init__(self, user):
+        self.user = user
+
     class Meta:
         model = Ticket
         fields = ['trans_id', 'region', 'issue_date', 'ticket_value',
                   'unit_price', 'ticket_count', 'balance', 'issuer']
-        widgets = {
-            'Issue Date': {'format': '%d.%m.%Y'},
-        }
 
     def get_queryset(self):
-        return Ticket.objects.all()
+        if self.user:
+            role = self.user.profile.role
+            print(f'User: {self.user}, Role: {role}')
+            if role:
+                if role.is_internal:
+                    return Ticket.objects.all()
+                else:
+                    region = self.user.companyuser.region
+                    print('Region: ', region)
+                    return Ticket.objects.filter(region=region)
+        print('Not allowed to export!')
+        return []
--- payments/views.py
+++ payments/views.py
@@ -7,10 +7,12 @@ from django.contrib.auth.models import User
 from .forms import IssueTicketForm
 from django.urls import reverse
 from django.contrib import messages
-from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
+from django.views.generic import (
+    ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.http import HttpResponse
 from .resources import PaymentResource, TicketResource
 from django.db.models import Q
+from datetime import datetime
 
 
 class PaymentsListView(LoginRequiredMixin, ListView):
@@ -42,13 +44,15 @@ class UserTicketingFormListView(LoginRequiredMixin, FormView):
         initial = super(UserTicketingFormListView, self).get_initial(**kwargs)
         company_id = self.kwargs.get('company_id')
         self.company = Company.objects.filter(pk=company_id).first()
-        self.success_url = reverse('user-ticketing', kwargs={'company_id': company_id})
+        self.success_url = reverse(
+            'user-ticketing', kwargs={'company_id': company_id})
         return initial
 
     def get_context_data(self, **kwargs):
         company_id = self.kwargs.get('company_id')
         kwargs['company'] = Company.objects.filter(pk=company_id).first()
-        kwargs['tickets'] = Ticket.objects.filter(issuer=self.request.user.companyuser).order_by('-issue_date')
+        kwargs['tickets'] = Ticket.objects.filter(
+            issuer=self.request.user.companyuser).order_by('-issue_date')
         return super(UserTicketingFormListView, self).get_context_data(**kwargs)
 
     def form_valid(self, form):
@@ -56,9 +60,11 @@ class UserTicketingFormListView(LoginRequiredMixin, FormView):
         data = form.cleaned_data
         print('Cleaned data: ', data)
         if data['trans_id'].strip() == 'ON-NET':
-            messages.warning(self.request, f'Failed, "ON-NET" is not valid Transaction ID')
+            messages.warning(
+                self.request, f'Failed, "ON-NET" is not valid Transaction ID')
         else:
-            payment = Payment.objects.filter(Q(payer_account=data['payer_account']), Q(trans_id=data['trans_id']) | Q(receipt_no=data['trans_id'])).first()
+            payment = Payment.objects.filter(Q(payer_account=data['payer_account']), Q(
+                trans_id=data['trans_id']) | Q(receipt_no=data['trans_id'])).first()
             if payment:
                 print('Payment: ', payment.amount)
                 cu = user.companyuser
@@ -66,17 +72,22 @@ class UserTicketingFormListView(LoginRequiredMixin, FormView):
                 count = int(payment.amount / unit_price)
                 balance = payment.amount - (count * unit_price)
                 if payment.ticket_issued:
-                    messages.warning(self.request, 'Alert, The ticket(s) for this payment already issued!')
+                    messages.warning(
+                        self.request, 'Alert, The ticket(s) for this payment already issued!')
                 elif not count:
-                    messages.warning(self.request, 'Fail, not enough amount to purchase a ticket!')
+                    messages.warning(
+                        self.request, 'Fail, not enough amount to purchase a ticket!')
                 else:
-                    ticket = Ticket(payment=payment, unit_price=unit_price, ticket_value=payment.amount, ticket_count=count, balance=balance, issuer=cu, region=cu.region)
+                    ticket = Ticket(payment=payment, unit_price=unit_price, ticket_value=payment.amount,
+                                    ticket_count=count, balance=balance, issuer=cu, region=cu.region)
                     ticket.save()
                     payment.ticket_issued = True
                     payment.save()
-                    messages.success(self.request, f'Success, you have successfully issued {count} ticket(s)!')
+                    messages.success(
+                        self.request, f'Success, you have successfully issued {count} ticket(s)!')
             else:
-                messages.warning(self.request, 'Failed, review your inputs and resubmit!')
+                messages.warning(
+                    self.request, 'Failed, review your inputs and resubmit!')
         return super().form_valid(form)
 
 
@@ -104,17 +115,18 @@ class AllTicketsListView(LoginRequiredMixin, ListView):
 
 
 def export_payments(request):
-    resource = PaymentResource()
+    resource = PaymentResource(request.user)
     dataset = resource.export()
     response = HttpResponse(dataset.csv, content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="payments.csv"'
+    export_id = datetime.now().strftime("%Y%m%d%H%M%S")
+    response['Content-Disposition'] = f'attachment; filename="{export_id}_Payments.csv"'
     return response
 
 
 def export_tickets(request):
-    resource = TicketResource()
-    # result = Ticket.objects.select_related('payment')
+    resource = TicketResource(request.user)
     dataset = resource.export()
     response = HttpResponse(dataset.csv, content_type='text/csv')
-    response['Content-Disposition'] = 'attachment; filename="tickets.csv"'
+    export_id = datetime.now().strftime("%Y%m%d%H%M%S")
+    response['Content-Disposition'] = f'attachment; filename="{export_id}_Tickets.csv"'
     return response
