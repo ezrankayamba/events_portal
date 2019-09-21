2578f9531b710986dddedb6b29e30897ff44a3b5
ezrankayamba <ezrankayamba@gmail.com>
2019-09-21 11:28:16 +0300

    Try email after add user

--- users/signals.py
+++ users/signals.py
@@ -9,18 +9,20 @@ from django.template.loader import render_to_string
 
 def notify_created(user):
     sub = 'Successful regitstraion'
-    html_content = render_to_string('users/created_user_mail.html', {'user': user})
+    html_content = render_to_string(
+        'users/created_user_mail.html', {'user': user})
     msg = f'You have been successfully created as username: {user.username}. Default password is Fiesta19. Events Portal is accessible at https://www.pincomtz.net'
     sender = 'Events Portal<nezatech.notifications@gmail.com>'
     to = [user.email]
-    send_mail(sub, msg, sender, to, fail_silently=False, html_message=html_content)
+    send_mail(sub, msg, sender, to, fail_silently=False,
+              html_message=html_content)
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
-        # notify_created(instance)
+        notify_created(instance)
 
 
 @receiver(post_save, sender=User)
