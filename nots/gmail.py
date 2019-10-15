1aacfe97dd2064fa1faf6d3ccb8797bcdc1a287a
ezrankayamba <ezrankayamba@gmail.com>
2019-10-15 12:37:03 +0300

    send mail to use gmailapi

--- nots/gmail.py
+++ nots/gmail.py
@@ -4,10 +4,29 @@ import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+import base64
 
 token_storage = 'events.token.pickle'
 cred_file = 'events_credentials.json'
 base_email = 'pincomtz.events'
+
+
+def send_message(service, message):
+    try:
+        return (service.users().messages().send(userId='me', body=message).execute())
+    except Exception as e:
+        print(f'An error occurred: {e}')
+
+
+def create_message(sender, to, subject, message_text):
+    message = MIMEText(message_text, 'html')
+    message['to'] = to
+    message['from'] = sender
+    message['subject'] = subject
+    raw = base64.urlsafe_b64encode(message.as_bytes())
+    raw = raw.decode()
+    return {'raw': raw}
 
 
 def init_service():
--- users/management/commands/sendmail.py
+++ users/management/commands/sendmail.py
@@ -1,5 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.core.mail import send_mail
+from nots import gmail
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
 
 
 class Command(BaseCommand):
@@ -7,7 +10,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("Running sendmail command!")
         sub = 'User created'
-        msg = 'Helloooooooo2'
-        sender = 'Events Portal<nezatech.notifications@gmail.com>'
-        to = ['ezrankayamba@gmail.com']
-        send_mail(sub, msg, sender, to, fail_silently=False,)
+        user = User.objects.get(username='fiesta.admin')
+        msg = render_to_string('users/created_user_mail.html', {'user': user})
+
+        sender = 'Events Portal<pincomtz.events@gmail.com>'
+        to = 'ezrankayamba@gmail.com'
+        # send_mail(sub, msg, sender, to, fail_silently=False,)
+        service = gmail.init_service()
+        message = gmail.create_message(sender, to, sub, msg)
+        print(message)
+        gmail.send_message(service, message)
